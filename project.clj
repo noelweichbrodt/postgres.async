3da3d9ab66f1eeca8cf88f25ee6b2da8dd0677a2
Noel Weichbrodt <noel@weichbrodt.org>
2017-05-23 18:14:21 -0400

    updated deps

--- project.clj
+++ project.clj
@@ -1,17 +1,14 @@
-(defproject alaisi/postgres.async "0.9.0-SNAPSHOT"
+(defproject alaisi/postgres.async "0.10.0-SNAPSHOT"
   :description "Asynchronous PostgreSQL Clojure client"
   :url "http://github.com/alaisi/postgres.async"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
         :url "http://github.com/alaisi/postgres.async.git"}
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.2.374"]
+  :dependencies [[org.clojure/clojure "1.9.0-alpha16"]
+                 [org.clojure/core.async "0.3.442"]
                  [com.github.alaisi.pgasync/postgres-async-driver "0.9"]
-                 [cheshire "5.6.1" :scope "provided"]]
+                 [cheshire "5.7.1" :scope "provided"]]
   :lein-release {:deploy-via :clojars}
   :global-vars {*warn-on-reflection* true}
-  :target-path "target/%s"
-  :profiles {:dev {:source-paths ["dev"]
-                   :dependencies [[org.clojure/tools.namespace "0.2.11"]
-                                  [org.clojure/java.classpath "0.2.3"]]}})
+  :target-path "target/%s")
